ade6c7b34a437d709a9d40e4edf947b6aba262f4
Eduardo Bier <dududcbier@gmail.com>
2017-04-29 16:15:34 -0300

    - Added search bar on the seminar list screen

--- app/src/main/java/iwasthere/android/ime/com/iwasthere/SeminarListActivity.java
+++ app/src/main/java/iwasthere/android/ime/com/iwasthere/SeminarListActivity.java
@@ -5,12 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -23,11 +30,89 @@ import java.util.ArrayList;
 public class SeminarListActivity extends AppCompatActivity {
 
     private ArrayList<Seminar> seminars;
+    private ArrayList<Seminar> allSeminars;
     private ListView seminar_list;
+    private SearchView mSearchView;
+    private SeminarsAdapter adapter;
 
-    public class SeminarsAdapter extends ArrayAdapter<Seminar> {
+    ActionMenuView actionMenuView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seminar_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionMenuView = (ActionMenuView) findViewById(R.id.action_menu_view);
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AddSeminarActivity.class);
+                startActivity(i);
+            }
+        });
+
+        String result = getIntent().getStringExtra("result");
+        try {
+            JSONObject jObj = new JSONObject(result);
+            JSONArray seminarsJSON = jObj.getJSONArray("data");
+            this.seminars = Seminar.getSeminars(seminarsJSON);
+            this.allSeminars = Seminar.getSeminars(seminarsJSON);
+        } catch (JSONException e) {
+            Log.e("ListActivity: ", "Invalid JSON returned from GET.");
+        }
+        this.seminar_list = (ListView) findViewById(R.id.seminar_list);
+        adapter = new SeminarsAdapter(this, seminars);
+        this.seminar_list.setAdapter(adapter);
+        seminar_list.setTextFilterEnabled(true);
+
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+        });
+        mSearchView.setSubmitButtonEnabled(false);
+        mSearchView.setQueryHint(getString(R.string.search_hint));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_teacher:
+            case R.id.my_account:
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public class SeminarsAdapter extends ArrayAdapter<Seminar> implements Filterable {
+
+        private ArrayList<Seminar> seminarList;
+        private ArrayList<Seminar> filteredSeminarList;
+
         public SeminarsAdapter(Context context, ArrayList<Seminar> seminars) {
             super(context, 0, seminars);
+            this.seminarList = seminars;
+            filteredSeminarList = seminars;
         }
 
         @Override
@@ -40,36 +125,56 @@ public class SeminarListActivity extends AppCompatActivity {
             name.setText(seminar.getName());
             return convertView;
         }
-    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_seminar_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), AddSeminarActivity.class);
-                startActivity(i);
-            }
-        });
-        String result = getIntent().getStringExtra("result");
-        try {
-            JSONObject jObj = new JSONObject(result);
-            JSONArray seminarsJSON = jObj.getJSONArray("data");
-            this.seminars = Seminar.getSeminars(seminarsJSON);
-        } catch (JSONException e) {
-            Log.e("ListActivity: ", "Invalid JSON returned from GET.");
+        @Override
+        public int getCount() {
+            return seminarList.size();
         }
-        this.seminar_list = (ListView) findViewById(R.id.seminar_list);
-        SeminarsAdapter adapter = new SeminarsAdapter(this, this.seminars);
-        this.seminar_list.setAdapter(adapter);
+
+        @Override
+        public Seminar getItem(int i) {
+            return seminarList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public Filter getFilter() {
+
+            final Filter filter = new Filter() {
+
+                @SuppressWarnings("unchecked")
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                    seminarList = (ArrayList<Seminar>) results.values;
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+
+                    FilterResults results = new FilterResults();
+                    filteredSeminarList = new ArrayList<Seminar>();
+
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < allSeminars.size(); i++) {
+                        if (allSeminars.get(i).getName().toLowerCase().contains(constraint)) {
+                            filteredSeminarList.add(allSeminars.get(i));
+                        }
+                    }
+                    results.count = filteredSeminarList.size();
+                    results.values = filteredSeminarList;
+
+                    return results;
+                }
+            };
+
+            return filter;
+        }
     }
-
-
 
 }
