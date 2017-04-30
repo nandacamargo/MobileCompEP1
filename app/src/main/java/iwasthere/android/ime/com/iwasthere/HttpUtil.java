5b9dfbc31fd7074225bd196c04c0c93cf9cf6499
Eduardo Bier <dududcbier@gmail.com>
2017-04-30 16:47:30 -0300

    Using Volley for our http requests - Login now uses volley

--- app/src/main/java/iwasthere/android/ime/com/iwasthere/HttpUtil.java
+++ app/src/main/java/iwasthere/android/ime/com/iwasthere/HttpUtil.java
@@ -3,6 +3,8 @@ package iwasthere.android.ime.com.iwasthere;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -20,6 +22,63 @@ import javax.net.ssl.HttpsURLConnection;
  */
 
 public class HttpUtil {
+
+    public static JSONObject getJSONObject(String response, String origin) {
+        JSONObject resp = null;
+        try {
+            resp = new JSONObject(response);
+        }
+        catch (JSONException e) {
+            resp = null;
+            Log.e(origin, "unable to parse JSON");
+        }
+        return resp;
+    }
+
+    public static JSONArray getJSONArray(String jArray, String origin) {
+        JSONArray resp = null;
+        try {
+            resp = new JSONArray(jArray);
+        }
+        catch (JSONException e) {
+            resp = null;
+            Log.e(origin, "unable to parse JSON");
+        }
+        return resp;
+    }
+
+    public static Boolean responseWasSuccess(JSONObject resp) {
+        if (resp == null) return false;
+        try {
+            if (resp.has("success"))
+                return resp.getBoolean("success");
+        } catch (JSONException e) {
+            Log.e("responseWasSuccess", "unable to parse JSON");
+        }
+        return false;
+    }
+
+    public static JSONArray getResponseDataArray(JSONObject resp) {
+        if (resp == null) return null;
+        try {
+            if (resp.has("data"))
+                return resp.getJSONArray("data");
+        } catch (JSONException e) {
+            Log.e("responseWasSuccess", "unable to parse JSON");
+        }
+        return null;
+    }
+
+    public static String getResponseDataString(JSONObject resp) {
+        if (resp == null) return null;
+        try {
+            if (resp.has("data"))
+                return resp.getString("data");
+        } catch (JSONException e) {
+            Log.e("responseWasSuccess", "unable to parse JSON");
+        }
+        return null;
+    }
 
     public static JSONObject doPost(String stringURL, String s) {
 
