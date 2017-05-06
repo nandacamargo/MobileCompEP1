69957ff6390a055ff5d7bffd469c17519b527f34
Fernanda de Camargo <fernandacamargo_24@hotmail.com>
2017-05-05 22:43:37 -0300

    Added exception treatment on ScanQrCodeActivity and implementation of onActivityResult; Added user permissions Put dependence on build.gradle

--- Android/app/src/main/java/iwasthere/android/ime/com/iwasthere/ScanQrCodeActivity.java
+++ Android/app/src/main/java/iwasthere/android/ime/com/iwasthere/ScanQrCodeActivity.java
@@ -1,9 +1,12 @@
 package iwasthere.android.ime.com.iwasthere;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
 
@@ -43,18 +46,51 @@ public class ScanQrCodeActivity  extends AppCompatActivity implements ZXingScann
 
     @Override
     public void handleResult(Result rawResult) {
-        // Do something with the result here
+
         // Prints scan results
         Log.d("result", rawResult.getText());
 
         // Prints the scan format (qrcode, pdf417 etc.)
         Log.d("result", rawResult.getBarcodeFormat().toString());
 
+        /*AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();*/
+
         //If you would like to resume scanning, call this method below:
         //mScannerView.resumeCameraPreview(this);
-        Intent intent = new Intent();
-        //intent.putExtra(AppConstants.KEY_QR_CODE, rawResult.getText());
-        //setResult(RESULT_OK, intent);
-        finish();
+
+        try {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+
+            startActivityForResult(intent, 0);
+
+        } catch (Exception e) {
+
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast.makeText(this, contents, Toast.LENGTH_LONG).show();
+                // Handle successful scan
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+            }
+        }
     }
 }
